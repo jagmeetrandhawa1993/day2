87b48a9987704e13a66dbe1bf6bc25ff7d50342d
jagmeetkaur <jagmeetrandhawa1993@gmail.com>
2019-03-28 19:24:55 -0400

    Login Android

--- app/src/main/java/com/example/day2/MainActivity.java
+++ app/src/main/java/com/example/day2/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.day2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edtEmail;
+    private EditText edtPassword;
+    private Button btnSignIn;
+    private Button btnClear;
+    private TextView lblMessage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        edtEmail = findViewById(R.id.editEmail);
+        edtPassword = findViewById(R.id.editPassword);
+        lblMessage = findViewById(R.id.lblMessage);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnClear = findViewById(R.id.btnClear);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edtEmail.getText().toString();
+                // Toast.makeText(getApplicationContext(), email, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, email, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
